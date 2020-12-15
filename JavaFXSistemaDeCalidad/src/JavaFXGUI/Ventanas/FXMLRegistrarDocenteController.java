0cdcbd3e265611887a85361e5e5d31670d422da3
tbc_h <tbc_h@DESKTOP-9VLS4EJ>
2020-12-15 16:12:32 -0600

    funcionalidad registrar docente agregada

--- JavaFXSistemaDeCalidad/src/JavaFXGUI/Ventanas/FXMLRegistrarDocenteController.java
+++ JavaFXSistemaDeCalidad/src/JavaFXGUI/Ventanas/FXMLRegistrarDocenteController.java
@@ -4,8 +4,13 @@
  */
 package JavaFXGUI.Ventanas;
 
+import conexionBD.ConectarBD;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -34,6 +39,7 @@ public class FXMLRegistrarDocenteController implements Initializable {
     private TextField tfContraseña;
     
     Alert mostrarAlerta;
+    boolean registroExitoso = true;
 
     
     @Override
@@ -92,14 +98,155 @@ public class FXMLRegistrarDocenteController implements Initializable {
         }
         
         if(esCorrecto){
-            registrarDocente(nombreAuxiliar, numeroPersonalAuxiliar, telefonoAuxiliar, correoAuxiliar, contraseñaAuxiliar);
-        }else{
-            
+            registrarAcademico(numeroPersonalAuxiliar, nombreAuxiliar, telefonoAuxiliar);
+            registrarRolAcademico(numeroPersonalAuxiliar);
+            int idRol = obtenerIdRolAcademico(numeroPersonalAuxiliar);
+            int idAcademico = obtenerIdAcademico(numeroPersonalAuxiliar);
+            if(idRol > 0 && idAcademico > 0){
+                registrarUsuario(correoAuxiliar, contraseñaAuxiliar, idRol, idAcademico);
+            }else{
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error", "No fue posible completar el registro, "
+                    + "intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
+            if(registroExitoso){
+                mostrarAlerta = Herramientas.creadorDeAlerta("Mensaje", "Registro exitoso", Alert.AlertType.INFORMATION);
+                mostrarAlerta.showAndWait();
+            }else{
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error", "No fue posible completar el registro, "
+                    + "intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
         }
     }
     
-    private void registrarDocente(String nombre, String numeroDePersonal, String telefono, String correo, String contraseña){
+    private void registrarAcademico(String numeroDePersonal, String nombre, String telefono){
+        Connection conn = ConectarBD.abrirConexionMySQL();
+        if(conn != null){
+            try{
+                String consulta = "INSERT INTO academico(numeroPersonal, nombre, telefono) VALUES (?, ?, ?)";
+                PreparedStatement declaracion = conn.prepareStatement(consulta);
+                declaracion.setString(1, numeroDePersonal);
+                declaracion.setString(2, nombre);
+                declaracion.setString(3, telefono);
+                int resultado = declaracion.executeUpdate();
+                if(resultado == 0){
+                    registroExitoso = false;
+                }
+                conn.close();
+            }catch(SQLException ex){
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error de consulta", "No fue posible acceder a la base de datos "
+                    + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
+        }else{
+            mostrarAlerta = Herramientas.creadorDeAlerta("Error de conexión", "No fue posible conectar con la base de datos"
+                + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+            mostrarAlerta.showAndWait();
+        }
+    }
+    
+    private void registrarRolAcademico(String numeroDePersonal){
+        Connection conn = ConectarBD.abrirConexionMySQL();
+        if(conn != null){
+            try{
+                String consulta = "INSERT INTO rol (tipoRol, numeroDePersonal) VALUES ('Docente', ?)";
+                PreparedStatement declaracion = conn.prepareStatement(consulta);
+                declaracion.setString(1, numeroDePersonal);
+                int resultado = declaracion.executeUpdate();
+                if(resultado == 0){
+                    registroExitoso = false;
+                }
+                conn.close();
+            }catch(SQLException ex){
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error de consulta", "No fue posible acceder a la base de datos "
+                    + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
+        }else{
+            mostrarAlerta = Herramientas.creadorDeAlerta("Error de conexión", "No fue posible conectar con la base de datos"
+                + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+            mostrarAlerta.showAndWait();
+        }
+    }
         
+    private int obtenerIdAcademico(String numeroDePersonal){
+        int idAcademico = 0;
+        Connection conn = ConectarBD.abrirConexionMySQL();
+        if(conn != null){
+            try{
+                String consulta = "SELECT idAcademico FROM academico WHERE numeroPersonal = ?";
+                PreparedStatement declaracion = conn.prepareStatement(consulta);
+                declaracion.setString(1, numeroDePersonal);
+                ResultSet resultado = declaracion.executeQuery();
+                if(resultado.next()){
+                    idAcademico = resultado.getInt("idAcademico");
+                }
+                conn.close();
+            }catch(SQLException ex){
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error de consulta", "No fue posible acceder a la base de datos "
+                    + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
+        }else{
+            mostrarAlerta = Herramientas.creadorDeAlerta("Error de conexión", "No fue posible conectar con la base de datos"
+                + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+            mostrarAlerta.showAndWait();
+        }
+        return idAcademico;
+    }
+    
+    private int obtenerIdRolAcademico(String numeroDePersonal){
+        int idRolAcademico = 0;
+        Connection conn = ConectarBD.abrirConexionMySQL();
+        if(conn != null){
+            try{
+                String consulta = "SELECT idRol FROM rol WHERE numeroDePersonal = ?";
+                PreparedStatement declaracion = conn.prepareStatement(consulta);
+                declaracion.setString(1, numeroDePersonal);
+                ResultSet resultado = declaracion.executeQuery();
+                if(resultado.next()){
+                    idRolAcademico = resultado.getInt("idRol");
+                }
+                conn.close();
+            }catch(SQLException ex){
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error de consulta", "No fue posible acceder a la base de datos "
+                    + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
+        }else{
+            mostrarAlerta = Herramientas.creadorDeAlerta("Error de conexión", "No fue posible conectar con la base de datos"
+                + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+            mostrarAlerta.showAndWait();
+        }
+        return idRolAcademico;
+    }
+    
+    private void registrarUsuario(String correo, String contraseña, int idRol, int idAcademico){
+        Connection conn = ConectarBD.abrirConexionMySQL();
+        if(conn != null){
+            try{
+                String consulta = "INSERT INTO usuario(correo, password, idRol, idAcademico) VALUES (?, ?, ?, ?)";
+                PreparedStatement declaracion = conn.prepareStatement(consulta);
+                declaracion.setString(1, correo);
+                declaracion.setString(2, contraseña);
+                declaracion.setInt(3, idRol);
+                declaracion.setInt(4, idAcademico);
+                int resultado = declaracion.executeUpdate();
+                if(resultado == 0){
+                    registroExitoso = false;
+                }
+                conn.close();
+            }catch(SQLException ex){
+                mostrarAlerta = Herramientas.creadorDeAlerta("Error de consulta", "No fue posible acceder a la base de datos "
+                    + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+                mostrarAlerta.showAndWait();
+            }
+        }else{
+            mostrarAlerta = Herramientas.creadorDeAlerta("Error de conexión", "No fue posible conectar con la base de datos"
+                + "en este momento, intente más tarde", Alert.AlertType.ERROR);
+            mostrarAlerta.showAndWait();
+        }
     }
     
 }

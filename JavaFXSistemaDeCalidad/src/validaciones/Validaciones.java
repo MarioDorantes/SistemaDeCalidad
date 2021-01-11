a94eae610c3b554405c9d5deaad5aeceb0bdf788
MarioDorantes <62160690+MarioDorantes@users.noreply.github.com>
2021-01-10 19:54:15 -0600

    Avance validaciones

--- JavaFXSistemaDeCalidad/src/validaciones/Validaciones.java
+++ JavaFXSistemaDeCalidad/src/validaciones/Validaciones.java
@@ -7,11 +7,7 @@ package validaciones;
 
 public class Validaciones {
     
-    /*Recomiendo poner aqui el metodo de validacion ya cuando funcione al 100, 
-    mientras en otro proyecto hay que probar para evitar conflictos en el repo*/
-    
     private boolean nombreValido;
-    
     private boolean fechaExamenValida;
     private boolean periodoValido;
     private boolean puntajeObtenidoValido;
@@ -24,12 +20,43 @@ public class Validaciones {
     
     
     public boolean validarNombre (String nombre) {
-        return nombreValido = nombre.matches("^([A-Z]{1}[a-z]+[ ]?){2,4}$");
+        return nombreValido = nombre.matches("^([A-ZÑ]{1}[a-zñ]+[ ]?){2,4}$");
     } 
     
+    public boolean validarFechaExamen (String fecha){
+        return fechaExamenValida = fecha.matches("^[0-9]{4}[-]{1}[0-9]{1,2}[-]{1}[0-9]{1,2}$");
+    }
     
- 
-   
+    public boolean validarPeriodo (String periodo){
+        return periodoValido = periodo.matches("^[0-9]{6}$");
+    }
     
+    public boolean validarPuntajeObtenido (String puntajeObtenido){
+        return puntajeObtenidoValido = puntajeObtenido.matches("^[0-9]{1,2}[.]{1}[0-9]{2}$");
+    }
     
+    public boolean validarNombreAcademia (String nombreAcademia){
+        return nombreAcademiaValido = nombreAcademia.matches("^([A-ZÑa-zñ]+[ ]?)+$");
+    }
+    
+    public boolean validarPrograma (String programa){
+        return programaValido = programa.matches("^[A-ZÑ]{4}[-]{1}[0-9]{2}[-]{1}[A-ZÑ]{1}[-]{1}[A-ZÑ]{2}$");
+    }
+    
+    public boolean validarNrc (String nrc){
+        return nrcValido = nrc.matches("^[0-9]{5}$");
+    }
+    
+    public boolean validarCreditos (String creditos){
+        return creditosValido = creditos.matches("^[0-9]{1,2}$");
+    }
+    
+    public boolean validarBloque (String bloque){
+        return bloqueValido = bloque.matches("^[1|2]{1}$");
+    }
+    
+    public boolean validarNombreDeLaEE (String nombreDeLaEE){
+        return nombreDeLaEEValido = nombreDeLaEE.matches("^([A-ZÑa-zñ]+[ ]?)+$");
+    }
+     
 }
